9f2a7522ac0da408d4e0f0d2d406e2fb96233588
Jorge Zapata <jorgeluis.zapata@gmail.com>
2011-06-22 18:41:07 +0000

    The generator is almost done but the concept is completely wrong
    how are we going to get the descriptor of something that is not registered yet? given that this should be part of a library ... is a recursive dependency! duh ... either way the code is useful, we'll need to add a specific parser later

--- src/bin/ender_generator.c
+++ src/bin/ender_generator.c
@@ -20,50 +20,104 @@
 
 /* The idea here is to generate the .c code from an ender descriptor
  * file  (.ender). This way we can avoid the need to parse the file
- * everytime whene ender initialize and register the different
+ * everytime when ender initialize and register the different
  * namespaces statically
  */
 const char *ns_name;
-const char *name = "element";
+const char *name;
+FILE *out;
 
 static void help(void)
 {
-	printf("Run: ender_generator INPUT OUTPUT\n");
+	printf("Run: ender_generator INPUT NAME OUTPUT\n");
 	printf("Where INPUT is the ender file to load\n");
+	printf("NAME is the element name to dump\n");
 	printf("and OUTPUT is the c file to save\n");
+}
+
+static void _tabs(int count)
+{
+	int i;
+
+	for (i = 0; i < count; i++)
+		fprintf(out, "\t");
+}
+
+static void _dump_container_recursive(Ender_Container *container, int level)
+{
+	Ender_Value_Type type;
+	int i;
+
+	type = ender_container_type_get(container);
+	_tabs(level);
+	fprintf(out, "Ender_Container *tmp%d;\n", level);
+	_tabs(level);
+	fprintf(out, "tmp = ender_container_new(ENDER_%s);\n", ender_value_type_name_get(type));
+	if (ender_container_is_compound(container))
+	{
+		Ender_Container *sub;
+		int i;
+
+		_tabs(level);
+		fprintf(out, "{\n");
+		for (i = 0; i < ender_container_compound_count(container); i++)
+		{
+			sub = ender_container_compound_get(container, i);
+			_tabs(level + 1);
+			fprintf(out, "{\n");
+			_dump_container_recursive(sub, level + 2);
+			_tabs(level + 2);
+			fprintf(out, "ender_container_add(tmp%d, tmp%d);\n", level, level + 2);
+			_tabs(level + 1);
+			fprintf(out, "}\n");
+		}
+		_tabs(level);
+		fprintf(out, "}\n");
+	}
+}
+
+static void _dump_container(Ender_Property *p)
+{
+	Ender_Container *container;
+
+	container = ender_property_container_get(p);
+	fprintf(out, "\t{\n");
+	_dump_container_recursive(container, 2);
+	fprintf(out, "\t\tec = tmp2;\n");
+	fprintf(out, "\t}\n");
 }
 
 static void _dump_property(Ender_Property *p, void *data)
 {
-	Ender_Container *container;
 	Ender_Property_Flag flags;
 	const char *property_name;
 
 	property_name = ender_property_name_get(p);
-	container = ender_property_container_get(p);
 	flags = ender_property_flags_get(p);
-	printf("\tp = ender_descriptor_property_add(d, \"%s\",\n", property_name);
+	_dump_container(p);
+	fprintf(out, "\tp = ender_descriptor_property_add(d, \"%s\",\n", property_name);
+	fprintf(out, "\t\t\tec,\n");
 	if (flags & ENDER_GET)
-		printf("\t\t\t%s_%s_%s_get\n", ns_name, name, property_name);
+		fprintf(out, "\t\t\t%s_%s_%s_get\n", ns_name, name, property_name);
 	else
-		printf("\t\t\tNULL,\n");
+		fprintf(out, "\t\t\tNULL,\n");
 	if (flags & ENDER_SET)
-		printf("\t\t\t%s_%s_%s_set\n", ns_name, name, property_name);
+		fprintf(out, "\t\t\t%s_%s_%s_set\n", ns_name, name, property_name);
 	else
-		printf("\t\t\tNULL,\n");
+		fprintf(out, "\t\t\tNULL,\n");
 	if (flags & ENDER_ADD)
-		printf("\t\t\t%s_%s_%s_add\n", ns_name, name, property_name);
+		fprintf(out, "\t\t\t%s_%s_%s_add\n", ns_name, name, property_name);
 	else
-		printf("\t\t\tNULL,\n");
+		fprintf(out, "\t\t\tNULL,\n");
 	if (flags & ENDER_REMOVE)
-		printf("\t\t\t%s_%s_%s_remove\n", ns_name, name, property_name);
+		fprintf(out, "\t\t\t%s_%s_%s_remove\n", ns_name, name, property_name);
 	else
-		printf("\t\t\tNULL,\n");
+		fprintf(out, "\t\t\tNULL,\n");
 	if (flags & ENDER_CLEAR)
-		printf("\t\t\t%s_%s_%s_clear\n", ns_name, name, property_name);
+		fprintf(out, "\t\t\t%s_%s_%s_clear\n", ns_name, name, property_name);
 	else
-		printf("\t\t\tNULL,\n");
-	printf("\t\t\t);\n");
+		fprintf(out, "\t\t\tNULL,\n");
+	fprintf(out, "\t\t\t);\n");
 }
 
 static void dump(void)
@@ -77,27 +131,27 @@ static void dump(void)
 	if (!descriptor) return;
 	ns = ender_descriptor_namespace_get(descriptor);
 	ns_name = ender_namespace_name_get(ns);
-	printf("void %s_%s_init(void)\n", ns_name, name);
-	printf("{\n");
-	printf("\tEnder_Namespace *ns;\n");
-	printf("\tEnder_Container *ec;\n");
-	printf("\tEnder_Property *ep;\n");
-	printf("\tEnder_Descriptor *parent = NULL;\n");
-	printf("\tstatic Ender_Descriptor *d = NULL;\n");
-	printf("\tif (d) return;\n");
-	printf("\tns = %s_namespace_get();\n", ns_name);
+	fprintf(out, "void %s_%s_init(void)\n", ns_name, name);
+	fprintf(out, "{\n");
+	fprintf(out, "\tEnder_Namespace *ns;\n");
+	fprintf(out, "\tEnder_Container *ec;\n");
+	fprintf(out, "\tEnder_Property *ep;\n");
+	fprintf(out, "\tEnder_Descriptor *parent = NULL;\n");
+	fprintf(out, "\tstatic Ender_Descriptor *d = NULL;\n");
+	fprintf(out, "\tif (d) return;\n");
+	fprintf(out, "\tns = %s_namespace_get();\n", ns_name);
 	/* the descriptor itself */
 	descriptor_type = ender_descriptor_type(descriptor);
 	descriptor_parent = ender_descriptor_parent(descriptor);
 	if (descriptor_parent)
 	{
-		printf("\tparent = ender_descriptor_find(\"%s\");\n", ender_descriptor_name_get(descriptor_parent));
+		fprintf(out, "\tparent = ender_descriptor_find(\"%s\");\n", ender_descriptor_name_get(descriptor_parent));
 	}
-	printf("\td = ender_namespace_descriptor_add(ns, \"%s\", %s_%s_new, parent, ENDER_%s);\n", ns_name, ns_name, name, ender_type_name_get(descriptor_type));
-	printf("\tif (!d) return;\n");
+	fprintf(out, "\td = ender_namespace_descriptor_add(ns, \"%s\", %s_%s_new, parent, ENDER_%s);\n", ns_name, ns_name, name, ender_type_name_get(descriptor_type));
+	fprintf(out, "\tif (!d) return;\n");
 	/* the properties */
 	ender_descriptor_property_list(descriptor, _dump_property, descriptor);
-	printf("}\n");
+	fprintf(out, "}\n");
 }
 
 int main(int argc, char **argv)
@@ -113,12 +167,14 @@ int main(int argc, char **argv)
 	/* we force to disable the parse */
 	ender_init(&myargc, &myargv);
 	/* now parse the file input output */
-	if (argc < 3)
+	if (argc < 4)
 	{
 		help();
 		return -1;
 	}
 	ender_parser_load(argv[1]);
+	name = argv[2];
+	out = fopen(argv[3], "w");
 	dump();
 	ender_shutdown();
 
