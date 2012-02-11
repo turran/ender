46b406f0236dde2cdb8b22122ea5d08dbf2682ea
Vincent Torri <vincent.torri@gmail.com>
2012-02-11 10:19:54 +0000

    Ender: autotools improvments

--- src/include/Ender.h
+++ src/include/Ender.h
@@ -18,8 +18,30 @@
 #ifndef _ENDER_H
 #define _ENDER_H
 
-#include "Eina.h"
-#include "Enesim.h"
+#include <Eina.h>
+#include <Enesim.h>
+
+#ifdef _WIN32
+# ifdef EFL_ENDER_BUILD
+#  ifdef DLL_EXPORT
+#   define EAPI __declspec(dllexport)
+#  else
+#   define EAPI
+#  endif /* ! DLL_EXPORT */
+# else
+#  define EAPI __declspec(dllimport)
+# endif /* ! EFL_ENDER_BUILD */
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EAPI __attribute__ ((visibility("default")))
+#  else
+#   define EAPI
+#  endif
+# else
+#  define EAPI
+# endif
+#endif
 
 /**
  * @mainpage Ender
