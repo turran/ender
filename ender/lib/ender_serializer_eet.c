da6167462836e0b587519bd3e9936927fa404781
Jorge Zapata <jorgeluis.zapata@gmail.com>
2013-01-01 14:18:30 +0000

    More ideas on how to serialize using eet

--- ender/lib/ender_serializer_eet.c
+++ ender/lib/ender_serializer_eet.c
@@ -50,16 +50,107 @@ typedef union _Ender_Serializer_Eet_Value_Data
 	void *ptr;
 } Ender_Serializer_Eet_Value_Data;
 
+/* description used for a property */
 typedef struct _Ender_Serializer_Eet_Value
 {
 	Ender_Serializer_Eet_Value_Type type;
 	Ender_Serializer_Eet_Value_Data data;
 } Ender_Serializer_Value;
+
+/* description of an element
+ * it is composed of a list of int -> value (static properties)
+ * or string -> value (dynamic properties) tuples
+ */
+typedef struct _Ender_Serializer_Element
+{
+	Eina_List *properties;
+} Ender_Serializer_Element;
 /*----------------------------------------------------------------------------*
  *                       The serializer interface                             *
  *----------------------------------------------------------------------------*/
-static Ender_Serializer _ender_serializer_eet = {
+static void * _serializer_eet_container_new(Ender_Container *c)
+{
+	switch (c->type)
+	{
+		/* for simple types, just marshal the Ender_Value */
+		case ENDER_BOOL:
+		case ENDER_UINT32:
+		case ENDER_COLOR:
+		case ENDER_ARGB:
+		case ENDER_INT32:
+		case ENDER_UINT64:
+		case ENDER_INT64:
+		case ENDER_DOUBLE:
+		case ENDER_STRING:
+		break;
 
+		/* for element types, marshal a list of eet values */
+		case ENDER_OBJECT:
+		case ENDER_STRUCT:
+		case ENDER_ENDER:
+		case ENDER_UNION:
+		break;
+
+		/* for lists, marshal a list of its sub container
+		 * for simple sub containers just wrap all the possible
+		 * simple types. For element types, use the element
+		 * description
+		 */
+		case ENDER_LIST:
+		break;
+
+		/* a pointer is a not known type, we can not marshal it */
+		/* for a value, we dont know yet :P */ 
+		case ENDER_VALUE:
+		case ENDER_POINTER:
+		ERR("value not supported yet");
+		break;
+	}
+}
+
+static void * _serializer_eet_element_marshal(Ender_Descriptor *d,
+		Ender_Element *e, unsigned int *len)
+{
+	/* iterate over the descriptor properties and marshal them */
+	/* iterate over the element properties only and marshal them */
+	/* finally encode it */
+	return NULL;
+}
+
+static Ender_Element * _serializer_eet_element_unmarshal(
+		Ender_Descriptor *d, void *data, unsigned int len)
+{
+	/* decode the data */
+	/* create an element */
+	/* iterate over the generate struct and set the properties */
+	return NULL;
+}
+
+static void * _serializer_eet_value_marshal(void *sd,
+		const Ender_Value *v, unsigned int *len)
+{
+	/* check if the descriptor is a basic descriptor */
+	/* if so, encode directly */
+	/* if it is a value that holds an element, check the
+	 * descriptor constraint and marshal it if possible
+	 */
+	/* if it is a list, marshal it using its own descriptor */
+	return NULL;
+
+}
+
+static Ender_Value * _serializer_eet_value_unmarshal(
+		void *sd, void *data, unsigned int len)
+{
+	return NULL;
+}
+
+static Ender_Serializer _ender_serializer_eet = {
+	/* .container_new	= */ _serializer_eet_container_new,
+	/* .element_marshal	= */ _serializer_eet_element_marshal,
+	/* .element_unmarshal	= */ _serializer_eet_element_unmarshal,
+	/* .value_marshal	= */ _serializer_eet_value_marshal,
+	/* .value_unmarshal 	= */ _serializer_eet_value_unmarshal,
 };
 #endif
 /*============================================================================*
@@ -72,10 +163,10 @@ Ender_Serializer * ender_serializer_eet_get(void)
 	if (!_init++)
 	{
 		eet_init();
-		/* create the common eet descriptors for basic types and
-		 * structs, unions, objects and ender elements. For list
-		 * we need to create the descriptor for each case
-		 *
+		/* we need a common eet descriptor for basic types
+		 * we need another eet descriptor for structs, unions and
+		 * enders
+		 * and lastly a new descriptor for each kind of list
 		 */
 
 	}
