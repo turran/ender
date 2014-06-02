93e7de3725d1c452a6659fc7e3576e2f27780fe1
Jorge Zapata <jorgelius.zapata@gmail.com>
2014-06-03 00:55:45 +0200

    Add the future lib item

---
+++ src/lib/ender_item_lib.c
@@ -0,0 +1,44 @@
+/* ENDER - Enesim's descriptor library
+ * Copyright (C) 2010 Jorge Luis Zapata
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+/*============================================================================*
+ *                                  Local                                     *
+ *============================================================================*/
+typedef struct _Ender_Item_Lib
+{
+	Ender_Item base;
+} Ender_Item_Lib;
+/*============================================================================*
+ *                                 Global                                     *
+ *============================================================================*/
+Ender_Item * ender_lib_new(void)
+{
+
+}
+
+void ender_item_lib_version_set(Ender_Item *i, int version)
+{
+
+}
+
+/*============================================================================*
+ *                                   API                                      *
+ *============================================================================*/
+EAPI int ender_item_lib_version_get(Ender_Item *i)
+{
+
+}
