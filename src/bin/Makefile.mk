96d562c1948c6552775c6bf0d5895043fd2e3b07
Jorge Zapata <jorgeluis.zapata@gmail.com>
2014-07-02 21:27:33 +0200

    Remove obsolete code
    Also rename the final binary

--- src/bin/Makefile.mk
+++ src/bin/Makefile.mk
@@ -1,9 +1,7 @@
 
 bin_PROGRAMS = \
 src/bin/ender-inspect \
-src/bin/ender_loader
-#src/bin/ender_generator
-#src/bin/ender_generator
+src/bin/ender-loader
 
 src_bin_ender_inspect_LDADD = \
 $(top_builddir)/src/lib/libender.la \
@@ -15,17 +13,6 @@ src_bin_ender_inspect_CPPFLAGS = \
 
 src_bin_ender_inspect_SOURCES = \
 src/bin/ender_inspect.c
-
-src_bin_ender_generator_CPPFLAGS = \
--I$(top_srcdir)/src/lib \
-@ENDER_CFLAGS@
-
-src_bin_ender_generator_LDADD = \
-$(top_builddir)/src/lib/libender.la \
-@ENDER_LIBS@
-
-src_bin_ender_generator_SOURCES = \
-src/bin/ender_generator.c
 
 src_bin_ender_loader_LDADD = \
 $(top_builddir)/src/lib/libender.la \
